993cabaf193514993835593c622a2c46d677b8e2
sakus <saku.saisa@gmail.com>
2014-01-02 14:17:42 +0200

    yhdeltä istumalta suoraan pseudokoodista toteutettu min_heap ilman minkään sortin testausta

--- src/fi/sakusaisa/tiralabra/MinBinaryHeap.java
+++ src/fi/sakusaisa/tiralabra/MinBinaryHeap.java
@@ -9,16 +9,90 @@ package fi.sakusaisa.tiralabra;
 public class MinBinaryHeap {
 
 	private GridCell[] heapArray;
+	private int heapSize;
 	
 	public MinBinaryHeap() {
-		// TODO Auto-generated constructor stub
+		heapArray = new GridCell[100];
+		heapSize = 0;
 	}
 
+	/**
+	 * Returns an object's parent in the heap.
+	 * @param index The index of the object whose parent you want.
+	 * @return The parent object.
+	 */
+	private int getParent(int index) {
+		return (index+1)/2;
+	}
+	
+	/**
+	 * Returns the right child of the object.
+	 * @param index The index of the object whose child you want.
+	 * @return The index of the child.
+	 */
+	private int getRightChild(int index) {
+		return 2 * (index+1);
+	}
+	
+	/**
+	 * Returns the left child of the object.
+	 * @param index The index of the object whose child you want.
+	 * @return The index of the child.
+	 */
+	private int getLeftChild(int index) {
+		return 2 * (index+1) + 1;
+	}
+	
+	/**
+	 * Makes sure the heap is in working order.
+	 * @param index The index in the array to start running heapify from.
+	 */
+	private void heapify(int index) {
+	
+		int smallest;
+		int left = getLeftChild(index);
+		int right = getRightChild(index);
+		GridCell tempCell;
+	
+		// check if the right child exists
+		if (right <= heapSize) {
+			
+			// compare the values of the children for the smaller value
+			if (heapArray[left].getMovementCost() < heapArray[right].getMovementCost())
+				smallest = left;
+			else
+				smallest = right;
+			
+			/* 
+			 * if the value in the current index is larger than the smallest child,
+			 * switch them and then call heapify again
+			 */
+			if (heapArray[index].getMovementCost() > heapArray[smallest].getMovementCost()) {
+				tempCell = heapArray[index];
+				heapArray[index] = heapArray[smallest];
+				heapArray[smallest] = tempCell;
+				heapify(smallest);
+			}
+				
+		}
+		
+		/* if there is no right child and the value of the left child is smaller, 
+		 * switch the values (no need to heapify in this case)
+		 */
+		else if (left == heapSize && heapArray[index].getMovementCost() > heapArray[left].getMovementCost()) {
+			tempCell = heapArray[index];
+			heapArray[index] = heapArray[left];
+			heapArray[left] = tempCell;
+		}
+		
+	}
+	
 	/**
 	 * Clears the heap of all data making it empty.
 	 */
 	public void clear() {
-		
+		heapArray = new GridCell[100];
+		heapSize = 0;
 	}
 	
 	/**
@@ -27,6 +101,17 @@ public class MinBinaryHeap {
 	 */
 	public void insert(GridCell gridCell) {
 		
+		// make the heap one slot bigger
+		heapSize++;
+		
+		// traverse the heap "upwards" until the correct position is found
+		int i = heapSize;
+		while (i > 0 && heapArray[getParent(i)].getMovementCost() > gridCell.getMovementCost()) {
+			heapArray[i] = heapArray[getParent(i)];
+			i = getParent(i);
+		}
+		heapArray[i] = gridCell;
+		
 	}
 	
 	/**
@@ -34,7 +119,17 @@ public class MinBinaryHeap {
 	 * @return the deleted GridCell
 	 */
 	public GridCell delMin() {
-		return null;
+		
+		// grab the cell from the top of the heap
+		GridCell min = heapArray[0];
+		
+		// move the last cell into the top, mark the heap as 1 cell smaller and heapify
+		heapArray[0] = heapArray[heapSize];
+		heapSize--;
+		heapify(0);
+		
+		// finally return the old top of the heap
+		return min;
 	}
 	
 	/**
@@ -42,6 +137,7 @@ public class MinBinaryHeap {
 	 * @return The GridCell currently on top of the heap.
 	 */
 	public GridCell findMin() {
-		return null;
+		return heapArray[0];
 	}
+	
 }
