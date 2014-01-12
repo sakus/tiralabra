136ac6a2e6995bd01033530bd55126e5773c7a6d
sakus <saku.saisa@gmail.com>
2014-01-12 14:52:25 +0200

    avl-puu

--- src/fi/sakusaisa/tiralabra/datastructures/AvlTree.java
+++ src/fi/sakusaisa/tiralabra/datastructures/AvlTree.java
@@ -2,6 +2,12 @@ package fi.sakusaisa.tiralabra.datastructures;
 
 import fi.sakusaisa.tiralabra.core.GridCell;
 
+/**
+ * An AVL tree implementation.
+ * 
+ * @author Saku Säisä
+ *
+ */
 public class AvlTree {
 
 	private AvlTreeNode root;
@@ -184,6 +190,213 @@ public class AvlTree {
 			
 			p.height = Math.max(p.left.height, p.right.height) + 1;
 			p = p.parent;
+			
+		}
+		
+	}
+	
+	private AvlTreeNode findMin(AvlTreeNode node) {	
+	
+		if (node == null)
+			return null;
+		
+		while (node.left != null)
+			node = node.left;
+		
+		return node;
+		
+	}
+	
+	/**
+	 * Removes the minimum value key form the tree.
+	 * 
+	 * @return The deleted GridCell.
+	 */
+	public GridCell delMin() {
+		
+		AvlTreeNode nodeToDelete = findMin(this.root);
+		
+		if (nodeToDelete.parent == null)
+			this.root = null;
+		else
+			nodeToDelete.parent.left = null;
+		
+		return nodeToDelete.key;
+		
+	}
+
+	/**
+	 * Searches the tree for a specific GriDcell starting from the root.
+	 * 
+	 * @param cellToFind The GridCell of which existance in the tree to check.
+	 * @return Returns the AvlTreeNode instance of the GridCell if found, null if not foun.
+	 */
+	public AvlTreeNode contains(GridCell cellToFind) {
+	
+		// starting from the root, keep going until the node to be checked is null
+		AvlTreeNode node = this.root;
+		while (node != null) {
+			
+			// move on to the left or the right child based on if the value is smaller or bigger
+			if (cellToFind.getMovementCost() < node.keyValue)
+				node = node.left;
+			else if (cellToFind.getMovementCost() > node.keyValue)
+				node = node.right;
+			
+			// if the value matches, take a better look if it's actually the object we're looking for
+			else {
+				
+				if (node.key == cellToFind)
+					return node;
+				else
+					return null;
+				
+			}
+			
+		}
+		
+		return null;
+		
+	}
+
+	/**
+	 * Checks whether or not the tree is empty.
+	 * 
+	 * @return True if the tree is empty, false if it's not.
+	 */
+	public boolean isEmpty() {
+		return (this.root == null);
+	}
+
+	private AvlTreeNode remove(AvlTreeNode pois) {
+		
+		AvlTreeNode vanh, lapsi, seur;
+		
+		if (pois.left == null && pois.right == null) {
+			
+			vanh = pois.parent;
+			
+			if (vanh == null) {
+				this.root = null;
+				return pois;
+			}
+			
+			if (pois == vanh.left) {
+				vanh.left = null;
+			}
+			
+			else
+				vanh.right = null;
+		
+			return pois;
+			
+		}
+		
+		else if (pois.left == null || pois.right == null) {
+			
+			if (pois.left != null)
+				lapsi = pois.left;
+			else 
+				lapsi = pois.right;
+			
+			vanh = pois.parent;
+			lapsi.parent = vanh;
+			
+			if (vanh == null) {
+				this.root = lapsi;
+				return pois;
+			}
+			
+			if (pois == vanh.left)
+				vanh.left = lapsi;
+			else
+				vanh.right = lapsi;
+			
+			return pois;
+			
+		}
+		
+		else if (pois.left != null && pois.right != null) {
+			
+			seur = findMin(pois);
+			pois.key = seur.key;
+			lapsi = seur.right;
+			vanh = seur.parent;
+			
+			if (vanh.left == seur)
+				vanh.left = lapsi;
+			else
+				vanh.right = lapsi;
+			
+			if (lapsi != null)
+				lapsi.parent = vanh;
+			
+			return seur;
+			
+		}
+		
+		else return null;
+		
+	}
+	
+	/**
+	 * Removes a specifid GridCell from the tree if it exists.
+	 * 
+	 * @param deleteNode the GridCell to delete.
+	 */
+	public void avlRemove(GridCell deleteNode) {
+		
+		AvlTreeNode foundNode = contains(deleteNode);
+		
+		if (foundNode == null)
+			return;
+		
+		AvlTreeNode pois = remove(foundNode);
+		AvlTreeNode p = pois.parent;
+		
+		AvlTreeNode vanhempi = p.parent;
+		AvlTreeNode alipuu = null;
+
+		while (p != null) {
+			
+			if (p.height <= -2 || p.height >= 2) {
+				
+				if (p.height <= -2) {
+									
+					if (p.left.left.height > p.left.right.height)
+						alipuu = rightRotate(p);
+					else
+						alipuu = leftRightRotate(p);
+					
+				}
+				
+				else if (p.height >= 2) {
+					
+					if (p.right.right.height > p.right.left.height)
+						alipuu = leftRotate(p);
+					else
+						alipuu = rightLeftRotate(p);
+					
+				}
+				
+				if (p == this.root) {
+					this.root = alipuu;
+					return;
+				}
+				
+				if (alipuu.keyValue < vanhempi.keyValue)
+					alipuu = vanhempi.left;
+				else
+					alipuu = vanhempi.right;
+				
+				p = vanhempi;
+			
+			}
+			
+			else {
+				p.height = Math.max(p.left.height, p.right.height) + 1;
+				p = p.parent;
+			}
 			
 		}
 		
--- src/fi/sakusaisa/tiralabra/datastructures/AvlTreeNode.java
+++ src/fi/sakusaisa/tiralabra/datastructures/AvlTreeNode.java
@@ -41,5 +41,5 @@ public class AvlTreeNode {
 		this.keyValue = this.key.getMovementCost();
 		return this.keyValue;
 	}
-	
+		
 }
